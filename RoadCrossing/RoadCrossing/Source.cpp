a4140e075e9a5bb3a467b81a3309a85409881c12
Thanh Giang <20127486@student.hcmus.edu.vn>
2021-12-10 05:49:30 +0700

    add class console

---
+++ RoadCrossing/RoadCrossing/Source.cpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include "Console.h"
+
+
+enum MOVING {
+	ILDE = 0,
+	UP = 1,
+	DOWN = 2,
+	LEFT = 3,
+	RIGHT = 4
+};
+
+MOVING moving;
+
+int main()
+{
+	FixConsoleWindow();
+	GetSizeOfConsole();
+	HideCursor();
+	return 0;
+}
